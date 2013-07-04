c028c154292bbcb13918819e772cd18ecf3e6b14
Leonardo Loch Zanivan <pangalz@gmail.com>
2013-07-03 22:24:08 -0400

    teste 2

--- hw1/hello.js
+++ hw1/hello.js
@@ -3,4 +3,4 @@ var outfile = "hello.txt";
 var out = "A startup is a business built to grow rapidly.\n";
 fs.writeFileSync(outfile, out);  
 console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
-
+//fim
